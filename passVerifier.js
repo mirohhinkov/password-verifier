fc421675887bccfc1cceac2968962c1458304eb6
Miroslav Hinkov <miroh1@gmail.com>
2022-01-17 22:01:08 +0000

    Task completed. passVerifier.js - password checker function, test file - spes/support/spec.js

---
+++ passVerifier.js
@@ -0,0 +1,35 @@
+module.exports = (pasw) => {
+  if (!pasw) return "Password can not be null";
+  const reCond = [
+    new RegExp(".{8,}"),
+    new RegExp("[A-Z]"),
+    new RegExp("[a-z]"),
+    new RegExp("[0-5]"),
+  ];
+  const errMessages = [
+    "Password should contain at least 8 characters",
+    "Password should contain at least 1 capital letter",
+    "Password should contain at least 1 lowercase letter",
+    "Password should contain at least 1 number",
+  ];
+  const matchCond = reCond.map((el) => el.test(pasw));
+  conds = matchCond.reduce((acc, el) => acc + el);
+  let msg = "";
+  for (let i = 0; i < 4; i++) {
+    if (!matchCond[i]) {
+      msg = errMessages[i];
+      break;
+    }
+  }
+  //Second feature
+  if (conds === 3) {
+    msg = "";
+  }
+
+  // Third feature
+
+  if (conds === 3 && !matchCond[2]) {
+    msg = errMessages[2];
+  }
+  return msg ? msg : true;
+};
---
+++ spec/support/spec.js
@@ -0,0 +1,41 @@
+const passVerifier = require("../../passVerifier");
+
+//Test for null
+test1 = passVerifier(null);
+describe("password verifyer", () => {
+  it("should be an error message: ", () => {
+    expect(test1).toBe("Password can not be null");
+  });
+});
+
+//Test for less then 8
+let test2 = passVerifier("FR90BG");
+describe("password verifyer", () => {
+  it("should be an error message: ", () => {
+    expect(test2).toBe("Password should contain at least 8 characters");
+  });
+});
+
+//Test for less then 8, but has small, capital and number
+let test3 = passVerifier("as1Bf");
+describe("password verifyer", () => {
+  it("should be true: ", () => {
+    expect(test3).toBe(true);
+  });
+});
+
+//Test for more then 8 no number
+test4 = passVerifier("aabbbCCDD");
+describe("password verifyer", () => {
+  it("should be true: ", () => {
+    expect(test4).toBe(true);
+  });
+});
+
+//Test for more then 8 no number, no capital
+let test5 = passVerifier("aahfalkjhfj");
+describe("password verifyer", () => {
+  it("should be an error message: ", () => {
+    expect(test5).toBe("Password should contain at least 1 capital letter");
+  });
+});
